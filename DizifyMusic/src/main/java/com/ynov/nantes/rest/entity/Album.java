bf8da712af2d4eb7695685f14afe2ec03ec2b68c
Killian Cambert <killian.cambert@coopeo.fr>
2020-11-12 11:56:28 +0100

    feat(Entities1)

--- DizifyMusic/src/main/java/com/ynov/nantes/rest/entity/Album.java
+++ DizifyMusic/src/main/java/com/ynov/nantes/rest/entity/Album.java
@@ -1,4 +1,58 @@
 package com.ynov.nantes.rest.entity;
 
+import javax.persistence.*;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Entité Album
+ *
+ * @author Joulie Antonin & Cambert Killian
+ */
+@Entity
+@Table(name = "album")
 public class Album {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer albumId;
+
+    private String albumNom;
+
+    private Date date;
+
+    @ManyToOne
+    private Set<Artiste> artiste = new HashSet<>();
+
+    public Integer getAlbumId() {
+        return albumId;
+    }
+
+    public void setAlbumId(Integer albumId) {
+        this.albumId = albumId;
+    }
+
+    public String getAlbumNom() {
+        return albumNom;
+    }
+
+    public void setAlbumNom(String albumNom) {
+        this.albumNom = albumNom;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Set<Artiste> getArtiste() {
+        return artiste;
+    }
+
+    public void setArtiste(Set<Artiste> artiste) {
+        this.artiste = artiste;
+    }
 }
--- DizifyMusic/src/main/java/com/ynov/nantes/rest/entity/Utilisateur.java
+++ DizifyMusic/src/main/java/com/ynov/nantes/rest/entity/Utilisateur.java
@@ -1,4 +1,34 @@
 package com.ynov.nantes.rest.entity;
 
+import javax.persistence.*;
+
+/**
+ * Entité Utilisateur
+ *
+ * @author Joulie Antonin & Cambert Killian
+ */
+@Entity
+@Table(name = "utilisateur")
 public class Utilisateur {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer utilisateurId;
+
+    private String email;
+
+    public Integer getUtilisateurId() {
+        return utilisateurId;
+    }
+
+    public void setUtilisateurId(Integer utilisateurId) {
+        this.utilisateurId = utilisateurId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
