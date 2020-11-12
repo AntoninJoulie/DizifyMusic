99a2f6ed4c9c1572ebc8aabcd69c44b38d42253d
Killian Cambert <killian.cambert@coopeo.fr>
2020-11-12 15:29:32 +0100

    feat(Controller/RepositoryArtiste)

--- DizifyMusic/src/main/java/com/ynov/nantes/rest/controller/ArtisteController.java
+++ DizifyMusic/src/main/java/com/ynov/nantes/rest/controller/ArtisteController.java
@@ -1,4 +1,62 @@
 package com.ynov.nantes.rest.controller;
 
+import com.ynov.nantes.rest.entity.Artiste;
+import com.ynov.nantes.rest.repository.ArtisteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
 public class ArtisteController {
+
+    private ArtisteRepository artisteRepository;
+
+    @Autowired
+    public ArtisteController(ArtisteRepository artisteRepository) {
+        this.artisteRepository = artisteRepository;
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/artiste", method = RequestMethod.GET, params = "artisteNom")
+    public List<Artiste> getArtistesByName(@RequestParam(value = "artisteNom", defaultValue = "") String artisteNom) {
+        List<Artiste> artistes = artisteRepository.findByName(artisteNom);
+        return artistes;
+    }
+
+    @ResponseBody
+    @GetMapping("/artiste")
+    public List<Artiste> getArtistes() {
+        return artisteRepository.findAll();
+    }
+
+    @ResponseBody
+    @GetMapping("/artiste/{id}")
+    public Artiste getArtisteById(final @PathVariable("id") Integer artisteId) {
+        try {
+            Optional<Artiste> artiste = artisteRepository.findById(Integer.valueOf(artisteId));
+            return artiste.get();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    @PostMapping("/artiste")
+    public Artiste addArtiste(@RequestBody Artiste artiste) {
+        Artiste addedArtiste = artisteRepository.save(artiste);
+        return addedArtiste;
+    }
+
+    @ResponseBody
+    @PutMapping("/artiste/{artisteId}")
+    public Artiste editArtiste(@RequestBody Artiste artiste) {
+        Artiste updatedArtiste = artisteRepository.save(artiste);
+        return updatedArtiste;
+    }
+
+    @ResponseBody
+    @DeleteMapping("artiste/{artisteId}")
+    public Artiste deleteArtisteById(final @PathVariable("artisteId") Integer artisteId) {
+        return artisteRepository.deleteById(artisteId);
+    }
 }
--- DizifyMusic/src/main/java/com/ynov/nantes/rest/repository/ArtisteRepository.java
+++ DizifyMusic/src/main/java/com/ynov/nantes/rest/repository/ArtisteRepository.java
@@ -1,4 +1,19 @@
 package com.ynov.nantes.rest.repository;
 
-public class ArtisteRepository {
+import com.ynov.nantes.rest.entity.Artiste;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ArtisteRepository extends JpaRepository<Artiste, Integer> {
+    /**
+     * Recherche un artiste selon son nom
+     *
+     * @param artisteNom
+     * @return livre
+     */
+    @Query("SELECT a FROM Artiste a WHERE a.artisteNom LIKE %:artisteNom")
+    public List<Artiste> findByName(@Param("artisteNom") String artisteNom);
 }
