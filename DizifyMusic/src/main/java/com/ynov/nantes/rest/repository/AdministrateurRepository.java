9057132b510d8821ee80441f7f343c6aab633f86
Killian Cambert <killian.cambert@coopeo.fr>
2020-11-13 11:21:39 +0100

    refacto

--- DizifyMusic/src/main/java/com/ynov/nantes/rest/repository/AdministrateurRepository.java
+++ DizifyMusic/src/main/java/com/ynov/nantes/rest/repository/AdministrateurRepository.java
@@ -6,16 +6,17 @@ import org.springframework.data.repository.query.Param;
 
 import com.ynov.nantes.rest.entity.Administrateur;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface AdministrateurRepository extends JpaRepository<Administrateur, Integer> {
 
     /**
-     * Recherche un administrateur par son id
+     * Recherche un administrateur par son email
      *
-     * @param adminId l'id
+     * @param email
      * @return un admin
      */
     @Query("SELECT a FROM  Administrateur a WHERE a.email LIKE :email")
-    public Optional<Administrateur> findById(@Param("adminId") Integer adminId);
+    public List<Administrateur> findAdminByEmail(@Param("email") String email);
 }
