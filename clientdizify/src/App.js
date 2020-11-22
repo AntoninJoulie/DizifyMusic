f3e3cf4007495cb06832b76b78e8ffd3027d26d9
Antonin Joulie <antonin.joulie@ynov.com>
2020-11-22 23:01:01 +0100

    Front playlist titre Utilisateur

--- clientdizify/src/App.js
+++ clientdizify/src/App.js
@@ -7,6 +7,9 @@ import {
 } from "react-router-dom";
 import Album from "./Components/Album";
 import Artiste from "./Components/Artiste";
+import Playlist from "./Components/Playlist";
+import Utilisateur from "./Components/Utilisateur";
+import Titre from "./Components/Titre";
 
 export default function App() {
   return (
@@ -16,6 +19,9 @@ export default function App() {
           <Link to="/">Home</Link>
           <Link to="/artistes">Artistes</Link>
           <Link to="/albums">Albums</Link>
+          <Link to="/playlists">Playlists</Link>
+          <Link to="/utilisateurs">Utilisateurs</Link>
+          <Link to="/titres">Titres</Link>
         </nav>
 
         <Switch>
@@ -24,6 +30,15 @@ export default function App() {
           </Route>
           <Route path="/albums">
             <Album />
+          </Route>
+          <Route path="/playlists">
+            <Playlist />
+          </Route>
+          <Route path="/utilisateurs">
+            <Utilisateur />
+          </Route>
+          <Route path="/titres">
+            <Titre />
           </Route>
           <Route path="/">
             <Home />
